2eddcf5b8be73f14c0e74b4dccacaf891f063ee4
ramirorc1-cyber <ramiro_rc1@tesch.edu.mx>
2026-01-20 13:55:01 -0600

    avanceal de la aplicación

--- app/src/main/java/com/example/barberleomx/MainActivity.kt
+++ app/src/main/java/com/example/barberleomx/MainActivity.kt
@@ -3,24 +3,18 @@ package com.example.barberleomx
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.rememberNavController
 import com.example.barberleomx.data.preferences.SessionPreferences
 import com.example.barberleomx.ui.navigation.AppNavigation
-
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+import com.example.barberleomx.ui.navigation.NavGraph
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // 🔹 SIMULACIÓN DE LOGIN
-        val sessionPrefs = SessionPreferences(this)
-
-        if (!sessionPrefs.isLoggedIn()) {
-            sessionPrefs.setLoggedIn(true)
-            sessionPrefs.setUserRole("CLIENTE") // o "BARBERO"
-        }
-
         setContent {
-            AppNavigation()
+            val navController = rememberNavController()
+            NavGraph(navController)
         }
     }
 }
