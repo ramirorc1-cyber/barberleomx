1ba682c6c08948ef27b4b1545a4fe5fb72af9ade
ramirorc1-cyber <ramiro_rc1@tesch.edu.mx>
2026-01-21 22:05:43 -0600

    git commit -m "DAO para insertar y consultar pagos"

--- app/src/main/java/com/example/barberleomx/ui/data/local/entity/dao/ServicePaymentDao.kt
+++ app/src/main/java/com/example/barberleomx/ui/data/local/entity/dao/ServicePaymentDao.kt
@@ -1,4 +1,19 @@
-package com.example.barberleomx.ui.data.local.entity.dao
+package com.example.barberleomx.data.local.dao
 
-class ServicePaymentDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.barberleomx.data.local.entity.ServicePaymentEntity
+
+@Dao
+interface ServicePaymentDao {
+
+    @Insert
+    suspend fun insertPayment(payment: ServicePaymentEntity)
+
+    @Query("SELECT * FROM payments ORDER BY date DESC")
+    suspend fun getAllPayments(): List<ServicePaymentEntity>
+
+    @Query("SELECT SUM(price) FROM payments")
+    suspend fun getTotalSpent(): Int?
 }
